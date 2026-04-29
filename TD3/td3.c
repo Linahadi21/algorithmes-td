2bf7ab8dc699ec5a7f0bf9c30f11472bac4ff794
Linahadi21 <lina.hadi@uit.ac.ma>
2026-04-30 00:06:43 +0100

    Update td3.c

--- TD3/td3.c
+++ TD3/td3.c
@@ -1,1 +1,30 @@
-// TD3 code
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int val;
+    struct node* next;
+}Node;
+
+void insert(Node**L,int v){
+    Node*n=malloc(sizeof(Node));
+    n->val=v;
+    n->next=*L;
+    *L=n;
+}
+
+void display(Node*L){
+    while(L){
+        printf("%d -> ",L->val);
+        L=L->next;
+    }
+    printf("NULL\n");
+}
+
+int main(){
+    Node*L=NULL;
+    insert(&L,5);
+    insert(&L,2);
+    insert(&L,8);
+    display(L);
+}
